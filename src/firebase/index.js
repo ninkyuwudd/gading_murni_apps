030b2a0725b2e20ce2f7275a20911ffc74f04f0a
leonurium <leorangga30@ymail.com>
2025-05-16 18:10:36 +0700

    add neccesary file before submission

--- src/firebase/index.js
+++ src/firebase/index.js
@@ -1,4 +1,11 @@
+import firebase from '@react-native-firebase/app';
 import Notification from './notification';
+import { firebaseConfig } from '../config/firebase.config';
+
+// Initialize Firebase if it hasn't been initialized yet
+if (!firebase.apps.length) {
+  firebase.initializeApp(firebaseConfig);
+}
 
 export default class Firebase {
   static Notification = Notification;
